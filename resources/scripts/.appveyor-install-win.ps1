71a011a1bca9f57d2876439d0a0b9cd02e210957
Martin Rotter <rotter@praktik.cz>
2017-10-31 10:17:11 +0100

    Appveyor.

---
+++ resources/scripts/.appveyor-install-win.ps1
@@ -0,0 +1,42 @@
+ls
+
+$OutputEncoding = New-Object -typename System.Text.UTF8Encoding
+chcp 65001
+$OutputEncoding
+
+git clone -q --depth=1 https://github.com/martinrotter/textilosaurus.wiki.git C:\textilosaurus-wiki
+git config --global credential.helper store
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
+git config --global user.email "rotter.martinos@gmail.com"
+git config --global user.name "martinrotter"
+
+
+$file = (Get-ChildItem '*.7z').Name
+echo "File to upload: $file"
+$url = curl.exe --upload-file "$file" "https://transfer.sh/$file" --silent
+echo "Obtained URL: $url"
+
+$git_revision = git rev-parse --short HEAD
+$date = (Get-Date).ToUniversalTime().ToString("MM-dd-yyyy HH:mm:ss UTC")
+$webengine_type = if ($file -like '*nowebengine*') { echo "false" } else { echo "true" }
+
+echo "| $date | [$git_revision](https://github.com/martinrotter/textilosaurus/commit/$git_revision) | [transfer.sh (7z)]($url) | $webengine_type |  " | ac -Encoding "utf8" C:\textilosaurus-wiki\Windows-development-builds.md
+
+
+$file = (Get-ChildItem '*.exe').Name
+echo "File to upload: $file"
+$url = curl.exe --upload-file "$file" "https://transfer.sh/$file" --silent
+echo "Obtained URL: $url"
+
+$git_revision = git rev-parse --short HEAD
+$date = (Get-Date).ToUniversalTime().ToString("MM-dd-yyyy HH:mm:ss UTC")
+$webengine_type = if ($file -like '*nowebengine*') { echo "false" } else { echo "true" }
+
+echo "| $date | [$git_revision](https://github.com/martinrotter/textilosaurus/commit/$git_revision) | [transfer.sh (exe)]($url) | $webengine_type |  " | ac -Encoding "utf8" C:\textilosaurus-wiki\Windows-development-builds.md
+
+
+cd C:\textilosaurus-wiki
+git add *.*
+git commit -m "New files."
+git pull origin master
+git push origin master -v
