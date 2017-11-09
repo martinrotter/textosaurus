4fae7012741a74c05b59b82fbe372dcd3e9c0006
Martin Rotter <rotter.martinos@gmail.com>
2017-11-09 20:47:39 +0100

    Hide reset btn.

--- src/gui/settings/settingsexternaltools.cpp
+++ src/gui/settings/settingsexternaltools.cpp
@@ -2,10 +2,15 @@
 
 #include "gui/settings/settingsexternaltools.h"
 
+#include "miscellaneous/application.h"
+#include "miscellaneous/iconfactory.h"
+
 SettingsExternalTools::SettingsExternalTools(Settings* settings, QWidget* parent)
   : SettingsPanel(settings, parent) {
   m_ui.setupUi(this);
   m_ui.m_shortcut->hideResetButton();
+  m_ui.m_btnAdd->setIcon(qApp->icons()->fromTheme(QSL("list-add")));
+  m_ui.m_btnDelete->setIcon(qApp->icons()->fromTheme(QSL("list-remove")));
 }
 
 void SettingsExternalTools::loadSettings() {
