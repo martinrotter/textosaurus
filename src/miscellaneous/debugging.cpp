6dc666ffa0d6443085cec63ff276f8585b21fe83
Martin Rotter <rotter.martinos@gmail.com>
2017-12-20 13:26:16 +0100

    Use C++ function for debugging outputs.

--- src/miscellaneous/debugging.cpp
+++ src/miscellaneous/debugging.cpp
@@ -10,22 +10,29 @@
 #include <cstdio>
 #include <cstdlib>
 #include <ctime>
+#include <iomanip>
+#include <iostream>
 
-void Debugging::performLog(const char* message, QtMsgType type, const char* file, const char* function, int line) {
-  const char* type_string = typeToString(type);
+void Debugging::performLog(const QString& message, QtMsgType type, const char* file, const char* function, int line) {
+  auto type_string = typeToString(type);
 
   std::time_t t = std::time(nullptr);
-  char mbstr[32];
-
-  std::strftime(mbstr, sizeof(mbstr), "%y/%d/%m %H:%M:%S", std::localtime(&t));
+  std::tm tm = *std::localtime(&t);
 
   // Write to console.
-  if (file == 0 || function == 0 || line < 0) {
-    fprintf(stderr, "[%s] %s: %s (%s)\n", APP_LOW_NAME, type_string, message, mbstr);
+  if (file == nullptr || function == nullptr || line < 0) {
+    std::cerr << "[" << APP_LOW_NAME << "] "
+              << type_string.toUtf8().constData() << ": "
+              << message.toUtf8().constData() << " ("
+              << std::put_time(&tm, "%y/%d/%m %H:%M:%S") << ")" << std::endl;
   }
   else {
-    fprintf(stderr, "[%s] %s (%s)\n  Type: %s\n  File: %s (line %d)\n  Function: %s\n\n",
-            APP_LOW_NAME, message, mbstr, type_string, file, line, function);
+    std::cerr << "[" << APP_LOW_NAME << "] "
+              << message.toUtf8().constData() << " ("
+              << std::put_time(&tm, "%y/%d/%m %H:%M:%S") << ")" << std::endl
+              << "    Type: " << type_string.toUtf8().constData() << std::endl
+              << "    File: " << file << " (" << line << ")" << std::endl
+              << "    Function: " << function << std::endl;
   }
 
   if (type == QtFatalMsg) {
@@ -33,26 +40,26 @@ void Debugging::performLog(const char* message, QtMsgType type, const char* file
   }
 }
 
-const char* Debugging::typeToString(QtMsgType type) {
+QString Debugging::typeToString(QtMsgType type) {
   switch (type) {
     case QtDebugMsg:
-      return "DEBUG";
+      return QSL("DEBUG");
 
     case QtWarningMsg:
-      return "WARNING";
+      return QSL("WARNING");
 
     case QtCriticalMsg:
-      return "CRITICAL";
+      return QSL("CRITICAL");
 
     case QtFatalMsg:
     default:
-      return "FATAL (terminating application)";
+      return QSL("FATAL (terminating application)");
   }
 }
 
 void Debugging::debugHandler(QtMsgType type, const QMessageLogContext& placement, const QString& message) {
 #ifndef QT_NO_DEBUG_OUTPUT
-  performLog(qPrintable(message), type, placement.file, placement.function, placement.line);
+  performLog(message, type, placement.file, placement.function, placement.line);
 #else
   Q_UNUSED(type)
   Q_UNUSED(placement)
--- src/miscellaneous/debugging.h
+++ src/miscellaneous/debugging.h
@@ -13,8 +13,8 @@ class Debugging {
     // Specifies format of output console messages.
     // NOTE: QT_NO_DEBUG_OUTPUT - disables debug outputs completely!!!
     static void debugHandler(QtMsgType type, const QMessageLogContext& placement, const QString& message);
-    static void performLog(const char* message, QtMsgType type, const char* file = 0, const char* function = 0, int line = -1);
-    static const char* typeToString(QtMsgType type);
+    static void performLog(const QString& message, QtMsgType type, const char* file = 0, const char* function = 0, int line = -1);
+    static QString typeToString(QtMsgType type);
 };
 
 #endif // DEBUGGING_H
