88b5e20163511d819d844326e6c2400f76a608d2
Martin Rotter <rotter@praktik.cz>
2018-01-19 13:42:00 +0100

    Jasper pkg.

--- resources/scripts/.appveyor-prepare-win.sh
+++ resources/scripts/.appveyor-prepare-win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pacman -Syy --noconfirm
-pacman -S --noconfirm --needed perl ruby python2 mingw-w64-jasper mingw-w64-i686-toolchain mingw-w64-x86_64-toolchain git mingw-w64-x86_64-qt5-static
+pacman -S --noconfirm --needed perl ruby python2 mingw-w64-x86_64-jasper mingw-w64-i686-toolchain mingw-w64-x86_64-toolchain git mingw-w64-x86_64-qt5-static
