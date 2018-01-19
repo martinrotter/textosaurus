571f0e8c8f5cd05f315f3a724661da39fe9175a2
Martin Rotter <rotter@praktik.cz>
2018-01-19 09:10:36 +0100

    Try to build on AppVeyor.

--- resources/scripts/.appveyor-build-win.sh
+++ resources/scripts/.appveyor-build-win.sh
@@ -4,6 +4,6 @@ PATH="/mingw64/bin:$PATH"
 cd "/c/textilosaurus"
 mkdir "textilosaurus-build"
 cd "textilosaurus-build"
-qmake.exe "textilosaurus.pro"
+qmake.exe "C:\textilosaurus\textilosaurus.pro"
 mingw32-make
 mingw32-make windows_all
