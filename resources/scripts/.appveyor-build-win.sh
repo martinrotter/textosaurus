2c6eb5d16dd5fd48ef8c162dbfacb1b892d2b5c5
Martin Rotter <rotter@praktik.cz>
2018-01-19 12:05:34 +0100

    Allow -j.

--- resources/scripts/.appveyor-build-win.sh
+++ resources/scripts/.appveyor-build-win.sh
@@ -5,5 +5,5 @@ cd "/c/textilosaurus"
 mkdir "textilosaurus-build"
 cd "textilosaurus-build"
 qmake.exe "C:\textilosaurus\textilosaurus.pro" "CONFIG+=release"
-mingw32-make
+mingw32-make -j
 mingw32-make windows_all
