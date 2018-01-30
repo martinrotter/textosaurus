647e6bad630f7ff488a72b7ec30c4b0b99e79c15
Martin Rotter <rotter@praktik.cz>
2018-01-30 13:54:15 +0100

    Fix qmake call.

--- resources/scripts/.appveyor-build-win.ps1
+++ resources/scripts/.appveyor-build-win.ps1
@@ -4,11 +4,11 @@ cd "textilosaurus-build"
 
 $mak_path = "C:\textilosaurus\textilosaurus-build\Makefile"
 
-& 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat' amd64
+'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat'
 
 $env:PATH = "c:\textilosaurus\qt-5.10-static-msvc2017-ltcg-x86_64\bin\;" + $env:PATH
 
-& 'c:\textilosaurus\qt-5.10-static-msvc2017-ltcg-x86_64\bin\qmake.exe' "C:\textilosaurus\textilosaurus.pro" "CONFIG+=release"
+'c:\textilosaurus\qt-5.10-static-msvc2017-ltcg-x86_64\bin\qmake.exe' "C:\textilosaurus\textilosaurus.pro" "CONFIG+=release"
 
 $mak_contents = [System.IO.File]::ReadAllText($mak_path).Replace(" qjp2.lib", "")
 [System.IO.File]::WriteAllText($mak_path, $mak_contents)
