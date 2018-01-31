548090a8a357d2e14f12b5e1e7a85d8ba01cd5ed
Martin Rotter <rotter@praktik.cz>
2018-01-31 10:28:01 +0100

    Fix path.

--- resources/scripts/.appveyor-build-win.bat
+++ resources/scripts/.appveyor-build-win.bat
@@ -1,3 +1,2 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-powershell.exe "C:\textilosaurus\resources\scripts\.appveyor-build-win.ps1"
+powershell.exe -File "C:\textilosaurus\resources\scripts\.appveyor-build-win.ps1"
