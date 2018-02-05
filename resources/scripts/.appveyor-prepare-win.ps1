bbaf90a2ef1d5aaef3149bdf26c81adac91b68ac
Martin Rotter <rotter@praktik.cz>
2018-02-05 07:30:15 +0100

    Revert to MSVC appveyor.

---
+++ resources/scripts/.appveyor-prepare-win.ps1
@@ -0,0 +1,13 @@
+$qt_stub = "qt-5.10-static-ltcg-msvc2017-x86_64"
+
+$url = "https://bitbucket.org/skunkos/qt5-minimalistic-builds/downloads/$qt_stub.7z"
+$output = "qt.7z"
+
+cd C:\textilosaurus
+
+Invoke-WebRequest -Uri $url -OutFile $output
+
+& ".\resources\scripts\7za\7za.exe" x $output
+
+cd .\$qt_stub\bin
+& ".\qtbinpatcher.exe"
