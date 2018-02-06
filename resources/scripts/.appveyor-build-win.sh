3f57b89e4d26b4f20b05b00abf9ec4de49486dcc
Martin Rotter <rotter@praktik.cz>
2018-02-06 10:13:46 +0100

    Remove needless files.

--- resources/scripts/.appveyor-build-win.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PATH="/mingw64/qt5-static/bin:/mingw64/bin:$PATH"
-
-cd "/c/textilosaurus"
-mkdir "textilosaurus-build"
-cd "textilosaurus-build"
-qmake.exe "C:\textilosaurus\textilosaurus.pro" "CONFIG+=release"
-mingw32-make -j
-mingw32-make windows_all
