30df659fcc18645ddfe4ec041799ad9231042d1d
Martin Rotter <rotter@praktik.cz>
2018-04-16 10:48:36 +0200

    test modifications of json

--- resources/scripts/.flathub-release.sh
+++ resources/scripts/.flathub-release.sh
@@ -4,6 +4,16 @@
 # and push new appdata/flatpak-json to new branch.
 echo "flathub"
 
+commit_hash=$(git rev-parse HEAD)
+tag=$TRAVIS_TAG
+json_file="io.github.martinrotter.textosaurus.json"
+
 git clone https://martinrotter:${GH_TOKEN}@github.com/flathub/io.github.martinrotter.textosaurus.git ./flathub
 cd flathub
-cat *.*
+git checkout -b new-version
+
+# Replace old commit hash and branch.
+cat "$json_file" | sed -e "s@\"branch\": \".\+\"@\"branch\": \"$tag\"@g" > "$json_file"
+cat "$json_file" | sed -e "s@\"commit\": \".\+\"@\"commit\": \"$commit_hash\"@g" > "$json_file"
+
+cat "$json_file"
