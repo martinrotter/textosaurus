da76468b1de00468c625aad12e1cf4610feefbff
Martin Rotter <rotter@praktik.cz>
2018-04-16 10:54:21 +0200

    flathub test

--- resources/scripts/.flathub-release.sh
+++ resources/scripts/.flathub-release.sh
@@ -7,13 +7,14 @@ echo "flathub"
 commit_hash=$(git rev-parse HEAD)
 tag=$TRAVIS_TAG
 json_file="io.github.martinrotter.textosaurus.json"
+json_file_new="${json_file}.new"
 
 git clone https://martinrotter:${GH_TOKEN}@github.com/flathub/io.github.martinrotter.textosaurus.git ./flathub
 cd flathub
 git checkout -b new-version
 
 # Replace old commit hash and branch.
-cat "$json_file" | sed -e "s@\"branch\": \".\+\"@\"branch\": \"$tag\"@g" > "$json_file"
-cat "$json_file" | sed -e "s@\"commit\": \".\+\"@\"commit\": \"$commit_hash\"@g" > "$json_file"
+cat "$json_file" | sed -e "s@\"branch\": \".\+\"@\"branch\": \"$tag\"@g" > "$json_file_new"
+cat "$json_file_new" | sed -e "s@\"commit\": \".\+\"@\"commit\": \"$commit_hash\"@g" > "$json_file"
 
 cat "$json_file"
