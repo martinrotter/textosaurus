4cc5f1daee96e39a5b78b293fb5996a8a887e893
Martin Rotter <rotter@praktik.cz>
2018-05-07 08:30:36 +0200

    Upload DMG artifacts.

--- resources/scripts/.travis-install-osx.sh
+++ resources/scripts/.travis-install-osx.sh
@@ -12,3 +12,38 @@ make install
 # Make DMG image.
 make dmg
 otool -L "Textosaurus.app/Contents/MacOS/textosaurus"
+
+set -- *.dmg
+
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+dmgname="$1"
+dmgnamenospace="textosaurus-${git_tag_name}-${git_revision}-mac64.dmg"
+
+mv $dmgname $dmgnamenospace
+
+echo "File to upload: $dmgnamenospace"
+
+# DMG is finished, upload it.
+git config --global user.email "rotter.martinos@gmail.com"
+git config --global user.name "martinrotter"
+git clone https://martinrotter:${GH_TOKEN}@github.com/martinrotter/textosaurus.wiki.git ./build-wiki
+
+url=$(curl --upload-file "./$dmgnamenospace" "https://transfer.sh/$dmgnamenospace" --silent)
+
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+wikiline="| Mac OS X | $(date +'%m-%d-%Y %T') | [$git_revision](https\://github.com/martinrotter/textosaurus/commit/$git_revision) | [transfer.sh]($url) |  "
+wikifile="./build-wiki/Development-builds.md"
+wikifilenew="./build-wiki/Development-builds.md.new"
+
+echo "Line to add: $wikiline"
+cat "$wikifile" | sed -e "s@| Mac OS X | .\+ |  @$wikiline@g" > "$wikifilenew"
+
+cat "$wikifilenew"
+mv "$wikifilenew" "$wikifile"
+
+cd ./build-wiki
+git commit -a -m "New files."
+git pull origin master
+git push origin master
