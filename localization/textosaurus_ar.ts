8406094ed7453708fd0c063848cf8932c7d5ba36
martinrotter <martinrotter@users.noreply.github.com>
2018-07-11 08:34:39 +0200

    New translations textosaurus_en.ts (Arabic)

--- localization/textosaurus_ar.ts
+++ localization/textosaurus_ar.ts
@@ -1211,9 +1211,6 @@ You can install it now.</translation>
       <source>Cannot Open Update File</source>
       <translation type="unfinished">Cannot Open Update File</translation>
     </message>
-  </context>
-  <context>
-    <name>FormSettings</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/dialogs/formupdate.cpp" line="231"/>
       <source>Cannot open application update file. Update application manually.</source>
@@ -1301,9 +1298,6 @@ You can install it now.</translation>
       <source>Stop Recording</source>
       <translation type="unfinished">Stop Recording</translation>
     </message>
-  </context>
-  <context>
-    <name>IOFactory</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/plugin-system/macros/macrossidebar.cpp" line="49"/>
       <source>Play Recorded Macro</source>
@@ -1319,9 +1313,6 @@ You can install it now.</translation>
       <source>Stored Macros Toolbar</source>
       <translation type="unfinished">Stored Macros Toolbar</translation>
     </message>
-  </context>
-  <context>
-    <name>Macros</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/plugin-system/macros/macrossidebar.cpp" line="59"/>
       <source>Play Stored Macro</source>
@@ -1350,9 +1341,6 @@ You can install it now.</translation>
       <source>Recorder</source>
       <translation type="unfinished">Recorder</translation>
     </message>
-  </context>
-  <context>
-    <name>MacrosPlugin</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/plugin-system/macros/macroswidget.ui" line="83"/>
       <source>Recorded Actions</source>
@@ -1504,7 +1492,7 @@ You can install it now.</translation>
     <name>QObject</name>
     <message>
       <location filename="../src/libtextosaurus/common/miscellaneous/localization.cpp" line="27"/>
-      <location filename="../src/libtextosaurus/common/miscellaneous/localization.cpp" line="65"/>
+      <location filename="../src/libtextosaurus/common/miscellaneous/localization.cpp" line="66"/>
       <location filename="../src/libtextosaurus/saurus/miscellaneous/application.cpp" line="34"/>
       <source>LANG_ABBREV</source>
       <extracomment>Abbreviation of language, e.g. en. Use ISO 639-1 code here combined with ISO 3166-1 (alpha-2) code. Examples: &quot;cs&quot;, &quot;en&quot;, &quot;it&quot;, &quot;cs_CZ&quot;, &quot;en_GB&quot;, &quot;en_US&quot;.</extracomment>
@@ -1823,9 +1811,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>Select new text editor main font</source>
       <translation type="unfinished">Select new text editor main font</translation>
     </message>
-  </context>
-  <context>
-    <name>SettingsEditor</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingseditor.h" line="29"/>
       <source>Editor</source>
@@ -1954,9 +1939,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>Dump to output window</source>
       <translation type="unfinished">Dump to output window</translation>
     </message>
-  </context>
-  <context>
-    <name>SettingsGeneral</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingsexternaltools.cpp" line="31"/>
       <source>Copy to clipboard</source>
@@ -2113,9 +2095,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>(Tray icon is not available.)</source>
       <translation type="unfinished">(Tray icon is not available.)</translation>
     </message>
-  </context>
-  <context>
-    <name>SettingsLocalization</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingsgui.cpp" line="113"/>
       <source>Icon only</source>
@@ -2131,9 +2110,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>Text beside icon</source>
       <translation type="unfinished">Text beside icon</translation>
     </message>
-  </context>
-  <context>
-    <name>SettingsPlugins</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingsgui.cpp" line="116"/>
       <source>Text under icon</source>
@@ -2185,9 +2161,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>Name</source>
       <translation type="unfinished">Name</translation>
     </message>
-  </context>
-  <context>
-    <name>ShortcutCatcher</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingsplugins.cpp" line="16"/>
       <source>Author</source>
@@ -2203,9 +2176,6 @@ File filter for external e-mail selection dialog.</extracomment>
       <source>Plugins</source>
       <translation type="unfinished">Plugins</translation>
     </message>
-  </context>
-  <context>
-    <name>SyntaxColorThemeEditor</name>
     <message>
       <location filename="../src/libtextosaurus/saurus/gui/settings/settingsplugins.cpp" line="72"/>
       <source>no plugin selected</source>
