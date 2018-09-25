d3798efa44857030c64117bf88ce3414ff8d45ac
Martin Rotter <rotter@praktik.cz>
2018-09-25 10:29:26 +0200

    Use newer Qt.

--- resources/scripts/.appveyor-prepare-win.ps1
+++ resources/scripts/.appveyor-prepare-win.ps1
@@ -1,4 +1,4 @@
-$qt_stub = "qt-5.11.0-dynamic-msvc2017-x86_64"
+$qt_stub = "qt-5.11.2-dynamic-msvc2017-x86_64"
 
 $url = "https://bitbucket.org/skunkos/qt5-minimalistic-builds/downloads/$qt_stub.7z"
 $output = "qt.7z"
