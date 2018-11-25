ae5fc43f297f67683315868e656f8a9996af7900
Martin Rotter <rotter.martinos@gmail.com>
2018-11-25 14:43:03 +0100

    add new settings widget for encryption

--- src/libtextosaurus/saurus/gui/settings/settingsencryption.cpp
+++ src/libtextosaurus/saurus/gui/settings/settingsencryption.cpp
@@ -1,5 +1,6 @@
 // This file is distributed under GNU GPLv3 license. For full license text, see <project-git-repository-root-folder>/LICENSE.md.
 
+#include "common/miscellaneous/cryptofactory.h"
 #include "saurus/gui/settings/settingsencryption.h"
 
 SettingsEncryption::SettingsEncryption(Settings* settings, QWidget* parent) : SettingsPanel(settings, parent) {
@@ -8,6 +9,7 @@ SettingsEncryption::SettingsEncryption(Settings* settings, QWidget* parent) : Se
 
 void SettingsEncryption::loadSettings() {
   // Load values.
+  m_ui.m_txtStatus->setText(CryptoFactory::openSslVersion());
 
   //connect(m_ui.m_txtDecryption, &QPlainTextEdit::textChanged, this, &SettingsEncryption::dirtifySettings);
   //connect(m_ui.m_txtEncryption, &QPlainTextEdit::textChanged, this, &SettingsEncryption::dirtifySettings);
